01f7e70954ad6dafb0b6620aca4683d5ac7f7602
kuma <test>
2024-11-14 17:59:25 +0900

    회원가입 폼 및 my SQL 연동

---
+++ src/main/java/com/company/RegisterBean.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RegisterBean {
+	private String id;
+	private String pwd;
+	private String name;
+	private String num1;
+	private String num2;
+	private String email;
+	private String phone;
+	private String zipcode;
+	private String address;
+	private String job;
+}
